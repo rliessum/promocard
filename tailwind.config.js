65b21849c25a78dab5390925d4a8208c77c6ba29
Richard van Liessum <Richard@liessum.net>
2025-06-09 12:31:47 +0200

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './dist/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+        fontFamily: {
+            inter: ['Inter', 'sans-serif'],
+        },
+    },
+  },
+  plugins: [],
+}
